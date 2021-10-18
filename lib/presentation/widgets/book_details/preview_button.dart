ec75ae78a278e0a486db005bf4fdcb526de3a553
Alin-Gabriel Musat <musataling@gmail.com>
2021-10-18 14:28:11 +0300

    preview button spacing

--- lib/presentation/widgets/book_details/preview_button.dart
+++ lib/presentation/widgets/book_details/preview_button.dart
@@ -12,30 +12,33 @@ class PreviewButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-        style: OutlinedButton.styleFrom(
-          backgroundColor: Colors.black,
-          elevation: 4,
-          padding: const EdgeInsets.all(16),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(24.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            backgroundColor: Colors.black,
+            elevation: 4,
+            padding: const EdgeInsets.all(16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
           ),
-        ),
-        onPressed: () async {
-          if (book.previewLink != "") {
-            await canLaunch(book.previewLink)
-                ? await launch(book.previewLink)
-                : throw 'Could not launch ${book.previewLink}';
-          }
-        },
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32.0),
-            child: Text(
-              "Preview in Google Books",
-              style: Theme.of(context)
-                  .textTheme
-                  .button!
-                  .copyWith(color: Colors.white),
-            )));
+          onPressed: () async {
+            if (book.previewLink != "") {
+              await canLaunch(book.previewLink)
+                  ? await launch(book.previewLink)
+                  : throw 'Could not launch ${book.previewLink}';
+            }
+          },
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Text(
+                "Preview in Google Books",
+                style: Theme.of(context)
+                    .textTheme
+                    .button!
+                    .copyWith(color: Colors.white),
+              ))),
+    );
   }
 }
